8f2c8e4455dfcbcbb0e149fe3074878b3f1b1367
Mohammad Khan <mohammadkhan00@gmail.com>
2026-01-21 20:58:19 +0530

    Fix: update base path for deployment and clean warnings

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'learncodingfast', 
+  base: '/learncodingfast/', 
   plugins: [react()],
   build: {
     rollupOptions: {
